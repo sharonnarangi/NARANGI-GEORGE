b8de5109074b517785c0937b34e1388145196d5c
Sharon Nrangi <sharonnarangi@gmail.com>
2025-05-06 07:54:49 +0300

    `Added mobile menu toggle functionality and corresponding HTML structure`

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
 import './style.css'
 // Your JavaScript code here
 console.log('Vite + Tailwind CSS!');
+
+document.getElementById('menu-toggle').addEventListener('click', () => {
+    const menu = document.getElementById('mobile-menu');
+    menu.classList.toggle('hidden');
+  });
+  
