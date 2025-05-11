6c77682353774701ea5cdd9dcd98f4f8b1dfb27c
Sharon Nrangi <sharonnarangi@gmail.com>
2025-05-11 14:42:52 +0300

    Add: Adding theme colours to my Tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,4 +24,26 @@ export default {
       require('tailwindcss-smooth-scroll'),
     ],
   }
+
+  module.exports = {
+    content: [
+      './index.html', // or your entry file
+      './src/**/*.{js,ts,jsx,tsx}', // if you have a src directory
+    ],
+    theme: {
+      extend: {
+        colors: {
+          orange: {
+            100: '#FFE0B2', // light orange
+            200: '#FFB74D', // medium orange
+            400: '#FF8A00', // vibrant orange
+            500: '#FF6F00', // main orange
+            600: '#FF5722', // darker orange
+          }
+        }
+      }
+    },
+    plugins: [],
+  }
+  
   
